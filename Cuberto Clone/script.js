5a9de3869d491596067b57648e87f15742843936
Abhishek <yadavabhishek9503@gmail.com>
2023-10-10 01:20:27 +0530

    gsap added 1

--- Cuberto Clone/script.js
+++ Cuberto Clone/script.js
@@ -17,23 +17,23 @@ gsap.to(".fleftelm", {
   ease: Power1,
 });
 
-let sections = document.querySelectorAll(".fleftelm");
-Shery.imageEffect(".images", {
-  style: 4,
-  config: { onMouse: { value: 1 } },
-  slideStyle: (setScroll) => {
-    sections.forEach(function (section, index) {
-      ScrollTrigger.create({
-        trigger: section,
-        start: "top top",
-        scrub: 1,
-        onUpdate: function (prog) {
-          setScroll(prog.progress + index);
-        },
-      });
-    });
-  },
-});
+// let sections = document.querySelectorAll(".fleftelm");
+// Shery.imageEffect(".images", {
+//   style: 4,
+//   config: { onMouse: { value: 1 } },
+//   slideStyle: (setScroll) => {
+//     sections.forEach(function (section, index) {
+//       ScrollTrigger.create({
+//         trigger: section,
+//         start: "top top",
+//         scrub: 1,
+//         onUpdate: function (prog) {
+//           setScroll(prog.progress + index);
+//         },
+//       });
+//     });
+//   },
+// });
 
 
 
