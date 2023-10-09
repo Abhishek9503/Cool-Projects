d649aba2df63cf064a47e36f6361207483fcb990
Abhishek <yadavabhishek9503@gmail.com>
2023-10-10 01:12:56 +0530

    added gsap

--- Cuberto Clone/script.js
+++ Cuberto Clone/script.js
@@ -1,8 +1,8 @@
-Shery.mouseFollower();
-Shery.makeMagnet(".magnet");
-Shery.hoverWithMediaCircle(".hvr", {
-  videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
-});
+// Shery.mouseFollower();
+// Shery.makeMagnet(".magnet");
+// Shery.hoverWithMediaCircle(".hvr", {
+//   videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+// });
 
 gsap.to(".fleftelm", {
   scrollTrigger: {
@@ -34,3 +34,44 @@ Shery.imageEffect(".images", {
     });
   },
 });
+
+
+
+
+
+
+const cursor = new MouseFollower({
+    container: 'html',
+    speed: 0.3
+});
+
+// const cursor1 = new MouseFollower();
+// const el = document.querySelector('#homemain');
+
+// el.addEventListener('mouseenter', () => {
+//     cursor.setVideo('https://player.vimeo.com/external/479728625.sd.mp4?s=f4f886d3d45a0312d8d47419647788178535a2c6&profile_id=165&oauth2_token_id=57447761');
+// });
+
+// el.addEventListener('mouseleave', () => {
+//     cursor.removeVideo();
+// });
+
+
+
+// const cursor1 = new MouseFollower();
+const el = document.querySelector('#homemain');
+
+el.addEventListener('mouseenter', () => {
+    cursor.setText('Hello!');
+});
+
+el.addEventListener('mouseleave', () => {
+    cursor.removeText();
+});
+
+
+
+
+var Scrollbar = window.Scrollbar;
+
+Scrollbar.init(document.querySelector('#my-scrollbar'));
